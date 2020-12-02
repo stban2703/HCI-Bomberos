5c11d65cb4ecb607d9395f51f5c4a630325679ce
stban2703 <s.t.ban1401@gmail.com>
2020-12-02 10:43:19 -0500

    Added more particles

--- scripts/Particle.js
+++ scripts/Particle.js
@@ -8,6 +8,8 @@ class Particle {
 
     paint() {
         fill("#2FB2EB");
+        ellipse(this.posX + .4, this.posY + .4, this.width, this.height);
+        ellipse(this.posX + .2, this.posY + .2, this.width, this.height);
         ellipse(this.posX, this.posY, this.width, this.height);
     }
 }
